077bdd5bb3a3e4f8e54e9921f3b77211e161c27f
Marina-Golovanova <71488459+Marina-Golovanova@users.noreply.github.com>
2020-12-04 00:21:51 +0300

    Revert "lesson 2"

--- lesson-2/1.py
+++
@@ -1,3 +0,0 @@
-user = ("Marina", "Golovanova", 22, 169.5, None)
-for el in user:
-  print(el, type(el))
--- lesson-2/5.py
+++
@@ -1,6 +0,0 @@
-my_list = [7, 5, 3, 3, 2]
-while True:
-  el = int(input("Введите элемент рейтинга: "))
-  my_list.append(el)
-  my_list.sort(reverse=True)
-  print(my_list)
